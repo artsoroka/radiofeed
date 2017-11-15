5613fdd92ddd497e58336d2842f7f55a55790cbe
artsoroka <githubrepository@yandex.ru>
2017-11-15 18:36:09 +0000

    iterate over favorite shows and get episode list

--- parser.js
+++ parser.js
@@ -1,7 +1,31 @@
-const getPageBody = require('./lib/getPageBody'); 
-const getShowsList = require('./lib/getShowsList'); 
+const shows         = require('./lib/db/shows'); 
+const subscribtions = require('./lib/db/subscribtions'); 
+const getPageBody   = require('./lib/getPageBody'); 
+const getShowsList  = require('./lib/getShowsList'); 
+const getAudioLinks = require('./lib/getAudioLinks'); 
+const includes      = require('lodash/includes'); 
+const last          = require('lodash/last'); 
+const config        = require('./config'); 
 
-getPageBody(process.env.RADIOFEED_SHOWSLIST_URL)
-  .then(html => getShowsList(html))
-  .then(shows => console.log(shows)) 
-  .catch(err => console.log('ERROR: ', err)); 
+(async () => {
+  try{
+    var links = []; 
+    const showsPage = await getPageBody(config.urls.showsListUrl); 
+    const shows     = getShowsList(showsPage);   
+    const favorite  = await subscribtions.list(); 
+    
+    for(let show of shows.filter(s => includes(favorite, s.id))){
+      console.log('Cheking ' + show.id + '... '); 
+      const html = await getPageBody(config.urls.base + show.url); 
+      const audioFiles = getAudioLinks(html).filter(link => {
+        return last(link.split('/')).match(show.id); 
+      }); 
+      links = links.concat(audioFiles); 
+    }
+    
+    console.log(links); 
+    
+  } catch(e){
+    console.log('Error: ', e); 
+  }
+})();
