4547076150edb4b7040c60090bcd0ecaed751542
artsoroka <githubrepository@yandex.ru>
2017-11-20 23:53:08 +0300

    identify a problem if no shows are found

--- bin/initShows.js
+++ bin/initShows.js
@@ -1,10 +1,16 @@
-const shows       = require('../lib/db/shows'); 
-const getPageBody = require('../lib/getPageBody'); 
+const shows        = require('../lib/db/shows'); 
+const getPageBody  = require('../lib/getPageBody'); 
 const getShowsList = require('../lib/getShowsList'); 
-const config      = require('../config'); 
+const isEmpty      = require('lodash/isEmpty'); 
+const config       = require('../config'); 
 
 getPageBody(config.urls.showsListUrl)
   .then(html => getShowsList(html))
-  .then(showsList => shows.addBatch(showsList)) 
+  .then(showsList => {
+  	if( isEmpty(showsList) ){
+      throw new Error('could not fetch shows list'); 
+  	}
+  	shows.addBatch(showsList)
+   }) 
   .then(created => console.log(created))
   .catch(err => console.log('Could not initialize shows list: ', err)); 
