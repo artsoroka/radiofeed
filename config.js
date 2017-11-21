4bf7942433ac78c475fb26e5e847c875c5f28ce1
artsoroka <githubrepository@yandex.ru>
2017-11-21 16:02:22 +0000

    run check updates on schedule added node-schedule to run checks with intervals defined in config.worker section

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
+/*
+    *    *    *    *    *    *
+    ┬    ┬    ┬    ┬    ┬    ┬
+    │    │    │    │    │    |
+    │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+    │    │    │    │    └───── month (1 - 12)
+    │    │    │    └────────── day of month (1 - 31)
+    │    │    └─────────────── hour (0 - 23)
+    │    └──────────────────── minute (0 - 59)
+    └───────────────────────── second (0 - 59, OPTIONAL)
+*/
+
+var EVERY_15_MINUTES = '*/15 * * * *'; 
+
 module.exports = {
   database: {
     dir: './db', 
@@ -13,5 +27,8 @@ module.exports = {
   urls: {
     base: process.env.RADIOFEED_BASE_URL || 'http://radiofeed.com',
     showsListUrl: process.env.RADIOFEED_SHOWSLIST_URL || 'http://radiofeed.com/programs/'
+  }, 
+  worker: {
+    interval: process.env.RADIOFEED_CHECK_INTERVAL || EVERY_15_MINUTES
   }
 }; 
--- parser.js
+++ parser.js
@@ -1,51 +1,7 @@
-const Shows         = require('./lib/db/shows'); 
-const Subscribtions = require('./lib/db/subscribtions'); 
-const Downloads     = require('./lib/db/downloads'); 
-const isEmpty       = require('lodash/isEmpty'); 
-const checkUpdates  = require('./lib/checkNewEpisodes'); 
-const download      = require('./lib/download'); 
+const schedule     = require('node-schedule');
+const checkUpdates = require('./lib/checkUpdates'); 
+const config       = require('./config').worker; 
 
-(async () => {
-  try{
-    let links = []; 
-    const subscribed = await Subscribtions.list(); 
-    const shows      = await Shows.getShows({id: {$in: subscribed} }); 
-    
-    for(let show of shows){
-      console.log(`Cheking ${show.id}...`); 
-      const newEpisodes = await checkUpdates(show); 
-      
-      if( isEmpty(newEpisodes) ){
-        console.log(`No new episodes of ${show.id} found`); 
-        continue; 
-      }
-      
-      console.log(`New episodes of ${show.id} found`); 
-      const downloads = newEpisodes.map(episode => {
-        return {
-          show: {
-            title: show.title,
-            id: show.id
-          }, 
-          url: episode
-        }; 
-      }); 
-      links = links.concat(downloads); 
-      await Shows.addNewEpisodes(show, newEpisodes); 
-
-    }
-    
-    console.log(links); 
-    
-    for(let link of links){
-      const file = await download(link.url); 
-      await Downloads.addFile({
-        file: file, 
-        show: link.show
-      }); 
-    }
-    
-  } catch(e){
-    console.log('Error: ', e); 
-  }
-})();
+const j = schedule.scheduleJob(config.interval, async () => {
+  await checkUpdates(); 
+});
